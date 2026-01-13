44c90fc8c578243a7a73f9625577f7243abe15bd
eibay <eibayan@gmail.com>
2026-01-14 07:57:54 +1100

    chore: remove temporary test scripts and logs

--- start_mcp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# start_mcp.sh
-export MIX_ENV=dev
-export DIWA_DISABLE_WEB=true
-cd "$(dirname "$0")"
-
-# Use mix run via diwa.sh instead of escript to avoid NIF loading issues
-./diwa.sh start
