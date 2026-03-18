e44a6b906fb5ca19e3e96baeacb78d5ca17746df
eibay <eibayan@gmail.com>
2026-03-18 12:05:51 +1100

    fix: update migration paths in aliases to use deps/ path for CI compatibility

--- mix.exs
+++ mix.exs
@@ -33,10 +33,10 @@ defmodule DiwaAgent.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "ecto.migrate": [
-        "ecto.migrate --migrations-path ../diwa_schema/priv/repo/migrations --migrations-path priv/repo/migrations"
+        "ecto.migrate --migrations-path deps/diwa_schema/priv/repo/migrations --migrations-path priv/repo/migrations"
       ],
       "ecto.rollback": [
-        "ecto.rollback --migrations-path ../diwa_schema/priv/repo/migrations --migrations-path priv/repo/migrations"
+        "ecto.rollback --migrations-path deps/diwa_schema/priv/repo/migrations --migrations-path priv/repo/migrations"
       ],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
